901edea32e91269f20927028f3d4d0d403d5c246
Hilary Claire <hilary.wilson2563@gmail.com>
2019-09-17 20:58:34 -0400

    Updates

---
+++ precache-manifest.c2fe2863ae010f2f67e7b5c809974f91.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75ff8a7419daa7cfc425feabc802612f",
+    "url": "/primalroots/index.html"
+  },
+  {
+    "revision": "e30975bf82ce8baf4c9c",
+    "url": "/primalroots/static/css/main.17c2a90b.chunk.css"
+  },
+  {
+    "revision": "a9372c87345728d2dbc4",
+    "url": "/primalroots/static/js/2.0047a8ec.chunk.js"
+  },
+  {
+    "revision": "e30975bf82ce8baf4c9c",
+    "url": "/primalroots/static/js/main.35b2bd26.chunk.js"
+  },
+  {
+    "revision": "b45e57a3a055fe1dc4a7",
+    "url": "/primalroots/static/js/runtime~main.71a42cd3.js"
+  },
+  {
+    "revision": "4ce1ae7d3edb510e5ba5c84870ab607f",
+    "url": "/primalroots/static/media/Novecentoslabwide-Normal-webfont.4ce1ae7d.woff2"
+  },
+  {
+    "revision": "69f10bb13d8dbe018c668ae52bc7e198",
+    "url": "/primalroots/static/media/Novecentoslabwide-Normal-webfont.69f10bb1.woff"
+  },
+  {
+    "revision": "babec4dc649c2208fed149cbf85346da",
+    "url": "/primalroots/static/media/coverpage-01.babec4dc.png"
+  },
+  {
+    "revision": "3aa21426b87e32056b29abc8a1e5b6f5",
+    "url": "/primalroots/static/media/headshot.3aa21426.jpg"
+  },
+  {
+    "revision": "8faa3124ebc0c94a054464d311467121",
+    "url": "/primalroots/static/media/logo.8faa3124.png"
+  }
+]);
